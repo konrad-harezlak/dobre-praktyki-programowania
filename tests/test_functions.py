f6e00626b8bf92449b443feccc20eb818c089537
konrad-harezlak <konrad.harezlakk@gmail.com>
2025-10-28 15:18:00 +0100

    Testy dla funkcji word_frequencies #86c67a55h

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -44,3 +44,10 @@ def test_flatten_list():
     assert flatten_list([]) == []
     assert flatten_list([[[1]]]) == [1]
     assert flatten_list([1, [2, [3, [4]]]]) == [1, 2, 3, 4]
+    
+def test_word_frequencies():
+    assert word_frequencies("To be or not to be") == {"to": 2, "be": 2, "or": 1, "not": 1}
+    assert word_frequencies("Hello, hello!") == {"hello": 2}
+    assert word_frequencies("") == {}
+    assert word_frequencies("Python Python python") == {"python": 3}
+    assert word_frequencies("Ala ma kota, a kot ma Ale.") == {"ala": 1, "ma": 2, "kota": 1, "kot": 1, "ale": 1}
