59c1657716a0eea84f2270c4d64d091086958533
konrad-harezlak <konrad.harezlakk@gmail.com>
2025-10-28 15:04:42 +0100

    Dodanie testów dla funkcji fibonacci #86c67a55h

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -6,3 +6,11 @@ def test_is_palindrome():
     assert is_palindrome("python") == False
     assert is_palindrome("") == True
     assert is_palindrome("A") == True
+
+def test_fibonacci():
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(5) == 5
+    assert fibonacci(10) == 55
+    with pytest.raises(ValueError):
+        fibonacci(-1)
