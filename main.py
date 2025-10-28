80f253722a1eb0b3a5d3aa4b90ddc9ec9b61f28c
konrad-harezlak <konrad.harezlakk@gmail.com>
2025-10-28 14:46:18 +0100

    Implementacja funkcji is_prime #86c67a55h

--- main.py
+++ main.py
@@ -50,5 +50,11 @@ def word_frequencies(text: str) -> dict:
 
     return frequencies
 
-
+def is_prime(n: int) -> bool:
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
