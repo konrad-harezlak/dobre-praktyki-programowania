5f88360555d505a55b8e870eb8f404d9ac1b91ed
konrad-harezlak <konrad.harezlakk@gmail.com>
2025-10-28 15:17:41 +0100

    Testy dla funkcji flatten_list #86c67a55h

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -37,3 +37,10 @@ def test_calculate_discount():
         pass
     else:
         assert False, "Funkcja nie zgłosiła wyjątku dla zniżki > 1"
+
+def test_flatten_list():
+    assert flatten_list([1, 2, 3]) == [1, 2, 3]
+    assert flatten_list([1, [2, 3], [4, [5]]]) == [1, 2, 3, 4, 5]
+    assert flatten_list([]) == []
+    assert flatten_list([[[1]]]) == [1]
+    assert flatten_list([1, [2, [3, [4]]]]) == [1, 2, 3, 4]
