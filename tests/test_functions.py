f8faea226f8618e167cb9fd3aa24fa62e40eacad
konrad-harezlak <konrad.harezlakk@gmail.com>
2025-10-28 15:18:23 +0100

    Testy dla funkcji is_prime #86c67a55h

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -44,10 +44,19 @@ def test_flatten_list():
     assert flatten_list([]) == []
     assert flatten_list([[[1]]]) == [1]
     assert flatten_list([1, [2, [3, [4]]]]) == [1, 2, 3, 4]
-    
+
 def test_word_frequencies():
     assert word_frequencies("To be or not to be") == {"to": 2, "be": 2, "or": 1, "not": 1}
     assert word_frequencies("Hello, hello!") == {"hello": 2}
     assert word_frequencies("") == {}
     assert word_frequencies("Python Python python") == {"python": 3}
     assert word_frequencies("Ala ma kota, a kot ma Ale.") == {"ala": 1, "ma": 2, "kota": 1, "kot": 1, "ale": 1}
+
+def test_is_prime():
+    assert is_prime(2) == True
+    assert is_prime(3) == True
+    assert is_prime(4) == False
+    assert is_prime(0) == False
+    assert is_prime(1) == False
+    assert is_prime(5) == True
+    assert is_prime(97) == True
